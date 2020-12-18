2d2430d8ce6916e1c649fccbf4948a91163a2537
Philip Laxamana <philip.laxamana@hotmail.com>
2020-12-18 06:43:07 -0500

    extended the teal color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: ["src/**/*.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ["src/**/*.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                teal: colors.teal,
+            },
+            fontFamily: {
+                inter: ['Inter', 'sans-serif']
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography')
+    ],
 }
